ee7b8771304a4aa0b4fb60af94c8477410c8b6b8
Jamie Bowman <mrjamiebowman@gmail.com>
2021-01-12 18:38:25 -0500

    initial project

---
+++ src/KubernetesTutorial/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KubernetesTutorial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+    }
+}
