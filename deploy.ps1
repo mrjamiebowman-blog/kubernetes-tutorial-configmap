299fa99a73f2242375b3f3875a327109fdf92002
Jamie Bowman <mrjamiebowman@gmail.com>
2021-01-13 19:49:07 -0500

    creating service

--- deploy.ps1
+++ deploy.ps1
@@ -2,5 +2,6 @@ kubectl create namespace mrjamiebowman
 
 kubectl apply -f k8s/configmap.yaml
 kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
 
 kubectl get all -n mrjamiebowman
